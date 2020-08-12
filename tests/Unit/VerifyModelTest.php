a49937351256612cd258888c3431719b9f5057fe
Daniel Brendel <dbrendel1988@gmail.com>
2020-08-12 20:29:00 +0200

    Testcase for VerifyModel

---
+++ tests/Unit/VerifyModelTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+    Actifisys (dnyActifisys) developed by Daniel Brendel
+
+    (C) 2019 - 2020 by Daniel Brendel
+
+    Version: 1.0
+    Contact: dbrendel1988<at>gmail<dot>com
+    GitHub: https://github.com/danielbrendel/
+
+    Released under the MIT license
+*/
+
+namespace Tests\Feature\Models;
+
+use App\AppModel;
+use App\FavoritesModel;
+use App\CaptchaModel;
+use App\FaqModel;
+use App\User;
+use App\VerifyModel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class VerifyModelTest extends TestCase
+{
+    public function testVerifyStatus()
+    {
+        VerifyModel::verifyStatus(env('TEST_USERID'), VerifyModel::STATE_INPROGRESS);
+        $status = VerifyModel::getState(env('TEST_USERID'));
+        $this->assertEquals(VerifyModel::STATE_INPROGRESS, $status);
+
+        VerifyModel::verifyStatus(env('TEST_USERID'), VerifyModel::STATE_VERIFIED);
+        $status = VerifyModel::getState(env('TEST_USERID'));
+        $this->assertEquals(VerifyModel::STATE_VERIFIED, $status);
+
+        VerifyModel::verifyStatus(env('TEST_USERID'), VerifyModel::STATE_DECLINED);
+        $status = VerifyModel::getState(env('TEST_USERID'));
+        $this->assertEquals(VerifyModel::STATE_DECLINED, $status);
+    }
+}
