d0104fddfd9eced4e10a7091bf99cb7617419246
Daniel Brendel <dbrendel1988@gmail.com>
2021-03-30 21:15:46 +0200

    Year 2021

--- public/js/app.js
+++ public/js/app.js
@@ -19333,7 +19333,7 @@ module.exports = function(module) {
 /*
     Actifisys (dnyActifisys) developed by Daniel Brendel
 
-    (C) 2019 - 2020 by Daniel Brendel
+    (C) 2019 - 2021 by Daniel Brendel
 
     Version: 1.0
     Contact: dbrendel1988<at>gmail<dot>com
