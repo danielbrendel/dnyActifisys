78893473e4494c6560544e4c90afdf826c26e7dd
Daniel Brendel <dbrendel1988@gmail.com>
2021-09-23 22:11:41 +0200

    Firebase notification messaging

--- app/PushModel.php
+++ app/PushModel.php
@@ -64,6 +64,13 @@ class PushModel extends Model
             $entry->seen = false;
             $entry->userId = $userId;
             $entry->save();
+
+            if (env('FIREBASE_ENABLE', false)) {
+                $user = User::get($userId);
+                if (($user) && (isset($user->device_token)) && (is_string($user->device_token)) && (strlen($user->device_token) > 0)) {
+                    static::sendCloudNotification($shortMsg, $longMsg, $user->device_token);
+                }
+            }
         } catch (\Exception $e) {
             throw $e;
         }
@@ -134,6 +141,54 @@ class PushModel extends Model
                 $item->seen = true;
                 $item->save();
             }
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * Send cloud notification to Google Firebase
+     * 
+     * @param $title
+     * @param $body
+     * @param $device_token
+     * @return void
+     * @throws \Exception
+     */
+    private static function sendCloudNotification($title, $body, $device_token)
+    {
+        try {
+            $curl = curl_init();
+
+            $headers = [
+                'Content-Type: application/json',
+                'Authorization: key=' . env('FIREBASE_KEY')
+            ];
+
+            $data = [
+                'to' => $device_token,
+                'data' => [
+                    'title' => $title,
+                    'body' => $body,
+                    'icon' => asset('logo.png')
+                ]
+            ];
+
+            curl_setopt($curl, CURLOPT_URL, env('FIREBASE_ENDPOINT'));
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+            $result = curl_exec($curl);
+            $result_data = json_decode($result);
+            if ((!isset($result_data->success)) || (!$result_data->success)) {
+                //throw new \Exception('Failed to deliver Firebase cloud message');
+            }
+
+            curl_close($curl);
         } catch (\Exception $e) {
             throw $e;
         }
